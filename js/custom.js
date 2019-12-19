41c4594f9c5344ae0c035ab34c0bce2b68aa5935
gaze9999@iii <gaze9999@livemail.tw>
2019-12-19 12:14:02 +0800

    add ajax / not done

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,19 @@ let sInput = $('#sInput');
 let btnInput = $('#btn-add');
 
 btnInput.on('mouseup', function(e){
-    alert(sInput.val());
-})
+    // alert(sInput.val());
+    $.ajax({
+        type: "POST",
+        url: "./addStudent.php",
+        dataType: "json",
+        data:{
+            name: sInput.val()
+        }
+        // success: function() {
+            
+        // },
+        // error: function() {
+
+        // }
+    });
+});
